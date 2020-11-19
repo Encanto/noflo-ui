b1d1d352751df424e4147e364b19f87cabbaf195
WilliamRClark <cwilliamwork@yahoo.com>
2020-11-19 10:42:26 -0500

    Hide github integration

--- elements/noflo-account.js
+++ elements/noflo-account.js
@@ -160,7 +160,7 @@ Polymer({
       </div>
     </section>
     <section class="user">
-    <template is="dom-if" if="{{user.flowhub-user}}">
+    <template is="dom-if" if="{{false}}">
       <div>
         <template is="dom-if" if="{{user.flowhub-avatar}}">
           <div class="avatar"><img src="{{user.flowhub-avatar}}"></div>
@@ -175,14 +175,14 @@ Polymer({
         </div>
       </div>
     </template>
-    <template is="dom-if" if="{{!user.flowhub-user}}">
+    <template is="dom-if" if="{{false}}">
       <div class="toolbar">
         <a id="loginbutton" class="login" on-click="login">Login</a>
       </div>
     </template>
     </section>
     </nav>
-    <template is="dom-if" if="{{user.flowhub-user}}">
+    <template is="dom-if" if="{{false}}">
       <template is="dom-if" if="{{askForScope.length}}">
         <div class="banner">
           To be able to synchronize your GitHub projects, [[env.NOFLO_APP_TITLE]] needs repository access permissions. <a href="https://docs.flowhub.io/github-integration/" target="_blank">Read more</a>
